52fb3124b70e37580cee23574ef39885d47b1efa
Vopaaz <liyifan945@163.com>
2019-11-26 19:07:59 -0600

    Implement market

--- experiment/main.py
+++ experiment/main.py
@@ -1,19 +1,12 @@
 import sys
+
 sys.path.append(".")
 
-from experiment.market import Stock
+from experiment.market import Market
 from experiment.agent import GeneticBitAgent, BenchmarkAgent
 from experiment.config import TRAIN_START, TRAIN_END
 from experiment.data import read
 
 
-
-
 if __name__ == "__main__":
-    SYMBOL = "CMS"
-    a = GeneticBitAgent()
-    b = BenchmarkAgent(SYMBOL)
-    m = Stock(TRAIN_START, TRAIN_END, SYMBOL)
-    print(m.trade_by(a))
-    print(m.trade_by(b))
-
+    pass
--- experiment/market.py
+++ experiment/market.py
@@ -1,16 +1,38 @@
 import pandas as pd
-from experiment.data import read
+from experiment.data import read, ALL_SYMBOLS
 from experiment.config import *
 import glob
 import os
-from experiment.agent import Agent, GeneticAgent
+from experiment.agent import Agent, GeneticAgent, BenchmarkAgent
 import logging
 import numpy as np
-from util import KnowsFullTdf
+from experiment.util import KnowsFullTdf
 
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+class Market(KnowsFullTdf):
+    def __init__(self, start_date, end_date, all_symbols=ALL_SYMBOLS):
+        self.stocks = [Stock(start_date, end_date, symbol) for symbol in all_symbols]
+        self.benchmark = [
+            stock.trade_by(agent)
+            for stock, agent in zip(
+                self.stocks, [BenchmarkAgent(symbol) for symbol in all_symbols]
+            )
+        ]
+
+    def trade_by(self, agents):
+        assert isinstance(agents, list)
+        return [[stock.trade_by(agent) for stock in self.stocks] for agent in agents]
+
+    def evaluate(self, agents):
+        assert isinstance(agents, list)
+        return [
+            (np.array(raw_result) / self.benchmark).mean()
+            for raw_result in self.trade_by(agents)
+        ]
 
 
 class Stock(KnowsFullTdf):
@@ -18,7 +40,7 @@ class Stock(KnowsFullTdf):
         self.full_tdf = read(symbol)
         self.start_date = start_date
         self.end_date = end_date
-
+        self.symbol = symbol
 
     def trade_by(self, agent):
         assert isinstance(agent, Agent)
@@ -26,10 +48,10 @@ class Stock(KnowsFullTdf):
         agent_holding = False
         total_rev = 0
         logger.info(
-            f"Trading start for {agent.__class__.__name__}{', with gene'+ str(list(agent.gene)) if isinstance(agent, GeneticAgent) else ''}"
+            f"Trading '{self.symbol}' start for {agent.__class__.__name__}{', with gene'+ str(list(agent.gene)) if isinstance(agent, GeneticAgent) else ''}"
         )
         while today < self.end_date:
-            tdf = self.full_tdf.loc[: today]
+            tdf = self.full_tdf.loc[:today]
             decision = agent.decide(tdf)
             price = self.full_tdf.close.loc[today]
             if decision.buy() and not agent_holding:
@@ -49,4 +71,7 @@ class Stock(KnowsFullTdf):
 
 
 if __name__ == "__main__":
-    pass
+    SYMBOL = "CMS"
+    a = BenchmarkAgent(SYMBOL)
+    m = Market(TRAIN_START, TRAIN_END, [SYMBOL])
+    assert m.evaluate([a])[0] == 1
