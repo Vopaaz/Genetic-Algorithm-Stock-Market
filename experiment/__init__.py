d46d94e63428603a8a859401b2cc80a4d9eb282d
Vopaaz <liyifan945@163.com>
2019-11-28 20:19:22 -0600

    Correct epoch logic and add structured visualization

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -19,17 +19,33 @@ class Experiment(object):
         population = self.population
         result = []
         for i in range(epoch):
-            logger.info(f"Epoch {i+1}/{epoch} start.")
             evaluation = self.market.evaluate(population)
             best = max(evaluation)
             avg = np.mean(evaluation)
             result.append([best, avg])
-            logger.info(f"Epoch {i+1}'s best: {best}, average: {avg}")
+            logger.info(f"Generation {i}'s best: {best}, average: {avg}")
+
+            logger.info(f"Start evolving generation {i+1}/{epoch}.")
             population = self.evolution.evolve(population, evaluation)
 
         self.population = population
+        logger.info(f"Final population generated, start evaluation.")
+        self.train_eval = self.market.evaluate(population)
+        best = max(self.train_eval)
+        avg = np.mean(self.train_eval)
+        result.append([best, avg])
+        logger.info(f"Final population's best: {best}, average: {avg}")
+
         self.history = pd.DataFrame(result, columns=["best", "avg"])
         return self.history
+
+    def visualize(self):
+        assert hasattr(self, "history")
+        self.history.plot()
+        plt.xlabel("Epoch")
+        plt.ylabel("Performance")
+        plt.show()
+
 
 if __name__ == "__main__":
     from experiment.background.agent import GeneticBitAgent
@@ -38,19 +54,18 @@ if __name__ == "__main__":
     from experiment.util.config import TRAIN_START, TRAIN_END
     import matplotlib.pyplot as plt
 
-    population = [GeneticBitAgent() for _ in range(5)]
-    evolution = BitEvolution(0.8, 0.8, 0.2)
+    population = [GeneticBitAgent() for _ in range(10)]
+    evolution = BitEvolution(0.5, 0.6, 0.1)
     market = Market(TRAIN_START, TRAIN_END)
     e = Experiment(population, evolution, market)
-    res= e.run(5)
-    res.plot()
-    plt.show()
+    e.run(2)
+    e.visualize()
 
-    '''
+    """
     Problem in implementation: too slow.
     For 10 genetic bit agent, evaluation takes about 3 min.
     If a 100 agent and 50 epoch is used, it will take 1500 min,
     which is 25 hours to complete. This is unacceptable.
     With adding more financial rules, this time will grow even larger.
     Optimization needs to be done.
-    '''
+    """
